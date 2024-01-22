5e8d802240876e2f15dc6f34d20ef7e330ee34d6
Void-Individual <otayomitv@gmail.com>
2024-01-22 23:26:17 +0100

    betty update and big-O 3

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -12,27 +12,27 @@ int lomuto_partition(int *array, int low, int high, size_t size)
 {
 	int pivot = array[high];
 	int i = low - 1;
-    int j, temp;
+	int j, temp;
 
-    for (j = low; j <= high - 1; j++)
-    {
-        if (array[j] < pivot)
-        {
-            i++;
-            temp = array[i];
-            array[i] = array[j];
-            array[j] = temp;
-        }
-    }
+	for (j = low; j <= high - 1; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+		}
+	}
 
-    temp = array[i + 1];
-    array[i + 1] = array[high];
-    array[high] = temp;
+	temp = array[i + 1];
+	array[i + 1] = array[high];
+	array[high] = temp;
 
-    /* print the array after each swap */
-    print_array(array, size);
+	/* print the array after each swap */
+	print_array(array, size);
 
-    return (i + 1);
+	return (i + 1);
 }
 
 /**
@@ -42,15 +42,16 @@ int lomuto_partition(int *array, int low, int high, size_t size)
  * @high: Ending index of the partition to be sorted
  * @size: Size of the array
  */
+
 void quicksort(int *array, int low, int high, size_t size)
 {
-    if (low < high)
-    {
-        int pivot = lomuto_partition(array, low, high, size);
+	if (low < high)
+	{
+		int pivot = lomuto_partition(array, low, high, size);
 
-        quicksort(array, low, pivot - 1, size);
-        quicksort(array, pivot + 1, high, size);
-    }
+		quicksort(array, low, pivot - 1, size);
+		quicksort(array, pivot + 1, high, size);
+	}
 }
 
 /**
@@ -61,8 +62,8 @@ void quicksort(int *array, int low, int high, size_t size)
  */
 void quick_sort(int *array, size_t size)
 {
-    if (size < 2)
-        return;
+	if (size < 2 || array == NULL)
+		return;
 
-    quicksort(array, 0, size - 1, size);
+	quicksort(array, 0, size - 1, size);
 }
